10822addaccbb06354c1ab637d62e973ef27df61
Harshita2211 <guptaharshita979@gmail.com>
2026-04-06 02:06:28 +0530

    Webhooks Added

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
 
-    await mongoose.connect(process.env.MONGODB_URI, {
-  dbName: "bg-removal"
-});
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "bg-removal"
+    });
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
-}
+    isConnected = conn.connections[0].readyState;
+    console.log("Database Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export default connectDB
+export default connectDB;
